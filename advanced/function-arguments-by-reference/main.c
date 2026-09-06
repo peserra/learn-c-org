4308013707c40a3eecb3f43b59f2c1ae4379bba0
peserra <pedro.occulate@gmail.com>
2026-09-06 11:32:51 -0300

    exercise finished: dynamic allocation

--- advanced/function-arguments-by-reference/main.c
+++ advanced/function-arguments-by-reference/main.c
@@ -1,5 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main (){
+typedef struct {
+  char *name;
+  int age;
+} person;
+
+/* function declaration */
+void birthday(person * p)
+{
+  p->age++;
+}
+
+/* write your function here */
+
+int main() {
+  person john;
+  john.name = "John";
+  john.age = 27;
+
+  printf("%s is %d years old.\n", john.name, john.age);
+  birthday(&john);
+  printf("Happy birthday! %s is now %d years old.\n", john.name, john.age);
+
   return 0;
 }
