e6748167fc49143a8ee0c625d6a2eedcc2fede13
peserra <pedro.occulate@gmail.com>
2026-09-11 18:38:12 -0300

    pointer arithmetics solved, should add to the pointer each interaction

---
+++ advanced/pointer-arithmetics/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main() {
+  int intarray[5] = {10, 20, 30, 40, 50};
+  int *pointer = &intarray[2];
+  int *parray[3];
+
+  for (int i = 0; i < 3; ++i) {
+    parray[i] = pointer + i;
+  }
+
+  //test
+  
+  for (int i = 0; i < 3; ++i) {
+    if (parray[i] == &pointer[i]) {
+      printf("matched!\n");
+    }
+    else {
+      printf("Fail!\n");
+    }
+  }
+  return 0;
+}
